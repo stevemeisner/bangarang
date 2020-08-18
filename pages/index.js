565bcd414c1baf6054d30f770faae94196dfbe4f
Stephen Meisner <steve.meisner@anthroware.com>
2020-08-18 18:02:03 -0400

    Pulling Notion posts through a web worker into Next JS

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import { fetchNotionPosts } from '../lib/posts'
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export async function getStaticProps() {
+  let allPostsData = await fetchNotionPosts()
+
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Shout out to all the Lost Boys</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
