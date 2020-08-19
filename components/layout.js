80d904fd686d8c8a9ff2c07494dbd9cd15bf966a
Stephen Meisner <steve.meisner@anthroware.com>
2020-08-19 00:19:49 -0400

    Simple update to trigger fresh deploy.

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,7 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/favicon.ico" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="Notion, NextJS, a Cloudflare Worker, and the open source community walk into a bar..."
         />
         <meta
           property="og:image"
